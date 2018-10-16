ad09fcac41fa41c62d0d07f0b07bd0f84777c054
Nora Vogt <noraevogt@gmail.com>
2018-10-16 20:22:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,4 +18,10 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase === string) {
     return 'I can\'t hear you!'
   }
+    elseif (string.toUpperCase === string) {
+      return 'YES INDEED!'
+    }
+    else (string === 'I love you, Grandma.') {
+      return 'I love you, too.'
+    }
 }
