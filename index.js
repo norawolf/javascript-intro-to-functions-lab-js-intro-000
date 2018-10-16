62894f7a34e640102fff8c69f175fe0c6c00af1e
Nora Vogt <noraevogt@gmail.com>
2018-10-16 20:25:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,5 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return 'I can\'t hear you!';
-  } else (string.toUpperCase() === string) {
-    return 'YES INDEED!';
-  }
+  } 
 }
